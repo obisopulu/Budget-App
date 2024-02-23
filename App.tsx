9fa135f8ef3267b3e13e0a51c6030ffb8558b28c
obisopulu <obisopulu@gmail.com>
2024-02-23 17:05:30 +0100

    installation and configuration of dependencies, importation of database

--- App.tsx
+++ App.tsx
@@ -1,10 +1,46 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+
+import * as FileSystem from 'expo-file-system';
+import { Asset } from 'expo-asset';
+
+const loadDatabase = async () => {
+  const dbName = 'mySQLiteDB';
+  const dbAsset = require('./assets/mySQLiteDB.db');
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+
+  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+  if(!fileInfo.exists) {
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,
+      { intermediates: true }
+    )
+    await FileSystem.downloadAsync(dbUri, dbFilePath);
+  }
+}
 
 export default function App() {
+  const [dbLoaded, setDbLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    loadDatabase()
+      .then(() => setDbLoaded(true))
+      .catch((error) => console.error(error))
+  }, []);
+
+  if (!dbLoaded) 
+    return (
+      <View style={{flex:1}}>
+        <ActivityIndicator size="large" />
+        <Text>Loading...</Text>;
+      </View>
+    )
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app! xx</Text>
       <StatusBar style="auto" />
     </View>
   );
